3b49564d4c82dc4ee9c6db5cacaeea684d4bb021
Jc Dior <jessicaclairdior@gmail.com>
2020-09-19 23:46:16 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,27 @@
+class TicTacToe
+end
+
+def initialize
+  @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+end
+
+def WIN_COMBINATIONS
+[ 
+[0, 1, 2],
+[3, 4, 5], #Middle row
+[6, 7, 8], #Bottom row
+[0, 3, 6], #Left row
+[1, 4, 7], #Middle row
+[2, 5, 8], #Right row
+[0, 4, 8], #Left diagonal
+[2, 4, 6] #Right diagonal
+]
+end
+
+def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+end
